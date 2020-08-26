327b3def2d9afd324204cf7d447ebc0945ced0f1
mucahitaz <51249755+mucahitaz@users.noreply.github.com>
2020-08-26 20:42:38 +0300

    api file added,small changes to profile page,updated routes

--- app/forms.py
+++ app/forms.py
@@ -7,5 +7,8 @@ from app.models import Summoner
 
 class SummonerSearchForm(FlaskForm):
     summoner_name = StringField('Summoner Name', validators=[DataRequired()])
-    server = SelectField(u'Server', choices=[('tr', 'Turkey'), ('euw', 'Europe West')])
+    server = SelectField(u'Server', choices=[('TR1', 'Turkey'), ('EUW1', 'Europe W'),
+    	('EUN1', 'Europe NE'), ('NA1', 'North America'),
+    	('JP1', 'Japan'), ('LA1', 'Latin America 1'),
+    	('LA2', 'Latin America 2'), ('OC1', 'Oceania'), ('RU', 'Russia')])
     submit = SubmitField('Search')
